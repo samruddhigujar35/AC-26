4fc9bebf8e656fa6cef74ee09c663c72a77944be
samruddhigujar35 <samruddhigujar35@gmail.com>
2026-09-16 15:52:00 +0530

    Update Q1.java

--- Java OOP/Assignment 2/Q1.java
+++ Java OOP/Assignment 2/Q1.java
@@ -1,1 +1,34 @@
+class Parent
+{
+    void parentMethod()
+    {
+        System.out.println("This is parent class");
+    }
+}
 
+class Child extends Parent
+{
+    void childMethod()
+    {
+        System.out.println("This is child class");
+    }
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Parent p = new Parent();
+
+        Child c = new Child();
+
+        // 1. Parent method using parent object
+        p.parentMethod();
+
+        // 2. Child method using child object
+        c.childMethod();
+
+        // 3. Parent method using child object
+        c.parentMethod();
+    }
+}
