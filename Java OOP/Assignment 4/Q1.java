9e897ee41b15c062f9ddb3e3f7b5334eff9b6b0e
samruddhigujar35 <samruddhigujar35@gmail.com>
2026-09-24 19:42:24 +0530

    Create Q1.java

---
+++ Java OOP/Assignment 4/Q1.java
@@ -0,0 +1,10 @@
+public class Q1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
